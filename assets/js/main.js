aa62c9bb6704832b667ca7e92f473f3ca0f00d1d
Mateus Macedo <84480587+macedocedo@users.noreply.github.com>
2022-07-04 22:37:49 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,46 @@ const navLink = document.querySelectorAll('.nav_link')
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //-------------------
+
+/*=============== HOME SWIPER ===============*/
+let Swiper = new Swiper(".new-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+})
+
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: 'true',
+    spaceBetween: 16,
+}); 
+
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  }
+  window.addEventListener('scroll', scrollUp)
+  
+  
+  /*=============== SCROLL REVEAL ANIMATION ===============*/
+  const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+  })
+
+  
+
+  
